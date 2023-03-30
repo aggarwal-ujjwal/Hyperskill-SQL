61576883a85e21bb973959cdfce9b72ecef87dab
aggarwal-ujjwal <aggarwal.ujjwal.17@gmail.com>
2023-03-30 23:21:47 +0530

    Added stats example

---
+++ stats.sql
@@ -0,0 +1,14 @@
+-- Suppose you need to evaluate changes in the popularity of the name Jessie
+--between 1920 and 2000 (inclusive) based on census data.
+
+SELECT
+  year, gender, COUNT(*) as cnt
+FROM
+  census
+WHERE
+  year BETWEEN 1920 and 2000
+  AND name = 'Jessie'
+GROUP BY
+  year, gender
+ORDER BY
+  year, gender DESC;
